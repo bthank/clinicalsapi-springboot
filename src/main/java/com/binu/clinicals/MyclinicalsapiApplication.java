5c86b36764ddf35ad9af901dcd76f3402ae28236
bthank <bthankachan@yahoo.com>
2021-08-21 22:55:31 -0400

    Initial commit

---
+++ src/main/java/com/binu/clinicals/MyclinicalsapiApplication.java
@@ -0,0 +1,13 @@
+package com.binu.clinicals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyclinicalsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyclinicalsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binu/clinicals/MyclinicalsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binu.clinicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyclinicalsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
